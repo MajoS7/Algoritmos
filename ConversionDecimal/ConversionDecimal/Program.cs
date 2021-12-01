bbc7dbdee174cf6f37f9467076db1aa5eb24f08f
Majos7 <user.email>
2021-12-01 16:02:23 -0500

    Se agregaron dos algoritmos mas

---
+++ ConversionDecimal/ConversionDecimal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConversionDecimal
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Realizar un programa en C# que convierta un número del sistema de base 2 al sistema decimal.
+            Console.Write("Digite un numero Binario: ");
+            string binario = Console.ReadLine();
+            int cantidad = binario.Length;
+
+            int[] lista = new int[cantidad];
+
+            int contador = 0;
+            foreach (char item in binario)
+            {
+                lista[contador]=item;
+                contador = contador + 1;
+            }
+
+            contador = 0;
+            double numberdecimal = 0;
+
+            foreach (int i in lista)
+            {
+                
+                
+                if (Convert.ToInt32(i) == 49)
+                {
+                    numberdecimal = numberdecimal + Math.Pow(2, contador);
+                }
+                contador = contador + 1;
+            }
+
+
+            Console.WriteLine(numberdecimal); 
+
+        }
+    }
+}
---
+++ Serie/Serie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Serie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //​Realizar la siguiente serie:
+            // 11 12 13 14 15 21 22 23 24 25 31 32 33 34 35
+            Console.Write("Ingresé número:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int counter = 10;
+            for (int i=1; i<=number; i++)
+            {
+                counter = counter + 1;
+                Console.WriteLine($"{counter}");
+
+                if (counter % 5 == 0)
+                {
+                    counter = counter + 5;
+                }
+                
+            }
+            
+        }
+    }
+}
